bc2e112519990d7637a84849118cbe7eb840791d
Ronny Javier Soria Erazo <rsemuris2016@gmail.com>
2015-09-21 16:43:41 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Vamos a programar una calculadora
+import sys
 
-def main():
-    pass
+def to_number(num):
+    try:
+        if '.' in num:
+            return float(num)
+        else:
+            return int(num)
+    except:
+        sys.exit("Error")
 
-if __name__ == "__main__":
-    main()
+op1 = to_number(sys.argv[1])
+operation = sys.argv[2]
+op2 = to_number(sys.argv[3])
+
+def sum(num1, num2):
+    return num1 + num2
+def substraction(num1, num2):
+    return num1 - num2
+
+if operation == "suma":
+    print (sum(op1, op2))
+elif operation == "resta":
+    print (substraction(op1, op2))
+else:
+    print ("Not allowed operation ", operation)
