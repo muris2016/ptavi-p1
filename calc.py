b234f3a1bd82a3453862b0f22b2be91504f1c182
muris2016 <rsemuris2016@gmail.com>
2015-09-25 17:06:59 +0200

    if name y funcion do_operation añadidos

--- calc.py
+++ calc.py
@@ -3,6 +3,7 @@
 
 import sys
 
+
 def to_number(num):
     try:
         if '.' in num:
@@ -12,18 +13,26 @@ def to_number(num):
     except:
         sys.exit("Error")
 
-op1 = to_number(sys.argv[1])
-operation = sys.argv[2]
-op2 = to_number(sys.argv[3])
 
-def sum(num1, num2):
-    return num1 + num2
-def substraction(num1, num2):
-    return num1 - num2
+def sum(op1, op2):
+    return op1 + op2
 
-if operation == "suma":
-    print (sum(op1, op2))
-elif operation == "resta":
-    print (substraction(op1, op2))
-else:
-    print ("Not allowed operation ", operation)
+
+def substraction(op1, op2):
+    return op1 - op2
+
+
+def do_operation(operation, op1, op2):
+    if operation == "suma":
+        print (sum(op1, op2))
+    elif operation == "resta":
+        print (substraction(op1, op2))
+    else:
+        print ("Not allowed operation ", operation)
+
+if __name__ == "__main__":
+
+    op1 = to_number(sys.argv[1])
+    operation = sys.argv[2]
+    op2 = to_number(sys.argv[3])
+    do_operation(operation, op1, op2)
